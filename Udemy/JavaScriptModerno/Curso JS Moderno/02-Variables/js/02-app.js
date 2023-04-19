db0b9b025121adb5133304d18c3538f0b1e67ead
ITCOL\japaez <jacruz2@latam.stefanini.com>
2023-04-19 12:17:51 -0500

    Javascript moderno: uso de variables y strings

---
+++ Udemy/JavaScriptModerno/Curso JS Moderno/02-Variables/js/02-app.js
@@ -0,0 +1,9 @@
+//nueva forma de declarar variables LET y CONST
+
+// USANDO LET
+let nuevoProducto = 'tablet';
+nuevoProducto = 20;
+console.log("nuevoProducto let:", nuevoProducto);
+
+
+
---
+++ Udemy/JavaScriptModerno/Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,7 @@
+//*************** METODOS PARA LOS STRINGS */
+// Delimitar el string:
+
+
+const productos = "LLANTAS PIRELLI"
+console.log("longitud de la variable: " , productos.length)
+
