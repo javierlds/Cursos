8dc9dbe9b267c92a7505b59bc23da789239d9c25
ITCOL\japaez <jacruz2@latam.stefanini.com>
2023-04-26 19:40:35 -0500

    Continuacion curso javascript moderno uso de estructuras y de numeros

--- Udemy/JavaScriptModerno/Curso JS Moderno/04-Numeros/js/03-app.js
+++ Udemy/JavaScriptModerno/Curso JS Moderno/04-Numeros/js/03-app.js
@@ -3,24 +3,37 @@
 
 function realizaOperacion(operador){
     let resultado = 0;
-    var num01form = parseInt(document.getElementById("num_01").value);
-    var num02form = parseInt(document.getElementById("num_02").value);
+    var num01form = parseInt(document.getElementById("num01").value);
+    var num02form = parseInt(document.getElementById("num02").value);
 
     //if(operador === 1){ // === es una validacion de tipo y dato, == hace la conversion para la validacion
-    resultado = eval(num01form + operador + num02form);
+    if(operador != 'o'){
+        resultado = eval(num01form + operador + num02form); // eval permite ejecutar una expresion tipo string como expresion matematica
+        document.getElementById("resultado").value = resultado;
+    }
+}
+
+function redondeo(){
+    opcion = parseInt(prompt("Cual numero quiere redondear el 1 o el 2"));
+    resultado = opcion ===1 ?resultado = Math.round(num01form) :resultado = Math.round(num02form);
     document.getElementById("resultado").value = resultado;
 }
 
 function operador(){
-    const opciones = document.querySelectorAll('input[name="operador"]');
+    const operacionMat =["+","-","/","*"]
+    const opciones = document.querySelectorAll('input[name="radiooper"]'); // javier
     let seleccion;
-    opciones.forEach((opcion)=>{
-        if(opcion.checked){
-            seleccion = opcion.value;
-        }
-    });
-    debugger
-    realizaOperacion(document.getElementById("operador").value);
+
+    if(operacionMat.includes(opciones)){ // llama la funcion de solo operaciones matematicas
+        opciones.forEach((opcion)=>{   // de esta forma se recorre el radio buton y se almacena en opcion.
+            if(opcion.checked){ // opcion esta chequeado aplica la operacion matematica
+                seleccion = opcion.value;
+                realizaOperacion(seleccion);  /// vanesa
+            }
+        });
+    }else{
+        redondeo();
+    }
 }
 
 /*
