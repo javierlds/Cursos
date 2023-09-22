7844ce141b7620b2b3e1be464accfba803649fb1
Javier Paez <javierlds@gmail.com>
2023-09-21 22:11:29 -0500

    Avance curso java , java beans

---
+++ Udemy/Java_MasterCompleto/src/JavaBeans/EjemploJavaBean.java
@@ -0,0 +1,13 @@
+/***
+ *Implementa serializable permite enviar la clase entre diferentes servidores
+ * debe tener constructor sin argumentos
+ * atributos privados para que sean accedidos mediante metodos publicos
+ */
+
+
+package JavaBeans;
+
+import java.io.Serializable;
+
+public class EjemploJavaBean implements Serializable {
+}
